bded4e0378696e5d4b7222ac7011eed839776d72
Martin Howarth <mhowarth@onefile.co.uk>
2016-06-16 15:14:29 +0100

    Android -

--- src/android/AudioRecorder.java
+++ src/android/AudioRecorder.java
@@ -1,9 +1,6 @@
 package uk.co.onefile.nomadionic.audiocapture;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v7.Activity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.app.Activity;
 public class AudioRecorder extends Activity {
