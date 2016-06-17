6b2a8e012c79ccb1ca7b203acfe2a934db3dec97
Martin Howarth <mhowarth@onefile.co.uk>
2016-06-17 11:23:54 +0100

    Android - added mising imports.

--- src/android/AudioRecorder.java
+++ src/android/AudioRecorder.java
@@ -1,8 +1,41 @@
 package uk.co.onefile.nomadionic.audiocapture;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import android.os.Build;
+import android.util.Log;
+import android.content.Context;
+import android.content.Intent;
+
+import org.apache.cordova.file.FileUtils;
+import org.apache.cordova.file.LocalFilesystemURL;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.Config;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaHttpAuthHandler;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.LOG;
+import org.apache.cordova.PluginManager;
+import org.apache.cordova.PluginResult;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.Manifest;
+
 import android.os.Bundle;
 import android.view.View;
 import android.app.Activity;
+
 public class AudioRecorder extends Activity {
 
 	@Override
