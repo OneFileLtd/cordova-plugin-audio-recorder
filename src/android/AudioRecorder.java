cbc04295f805645d4c55065f8bec0381503566d3
Martin Howarth <mhowarth@onefile.co.uk>
2016-06-24 16:10:08 +0100

    removed appCompatActivity no longer required, using activity instead.

--- src/android/AudioRecorder.java
+++ src/android/AudioRecorder.java
@@ -8,7 +8,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
